0137554f015246f5201eac4007f028f3aa3d47df
MacLewis <44983313+MacLewis@users.noreply.github.com>
2018-11-16 23:38:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ const g_loveTable = [
 function jerry_analyse_incoming_love_message ( p_message )
 {
 	/* Si le message entrant provient de Zal ou Oli */
-	if ( /*( p_message.author.id === g_zal ) ||*/ ( p_message.author.id === g_oli ) /*|| ( p_message.author.id === g_mac )*/ )
+	if ( 0 )
 	{
 		/* Détermination du nombre d'élément dans le tableau */
 		l_love_table_length = g_loveTable.length;
